7e0434ca42d3e609643ea26147d636874a772f3b
KazakovaNet <kazakovaNet@yandex.ru>
2019-08-26 00:10:16 +0300

    MockTableView

--- TDDToDo/Controller/DataProvider.swift
+++ TDDToDo/Controller/DataProvider.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-enum Section: Int {
+enum Section: Int, CaseIterable {
     case todo
     case done
 }
@@ -41,10 +41,23 @@ extension DataProvider: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return TaskCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: TaskCell.self), for: indexPath) as! TaskCell
+        
+        guard let section = Section(rawValue: indexPath.section) else { fatalError() }
+        guard let taskManager = taskManager else { fatalError() }
+        
+        let task: Task
+        switch section {
+        case .todo: task = taskManager.task(at: indexPath.row)
+        case .done: task = taskManager.doneTask(at: indexPath.row)
+        }
+        
+        cell.configure(withTask: task)
+        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return Section.allCases.count
     }
 }
